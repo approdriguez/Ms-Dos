cc3988332134c0e3999c2030cf23235e97f6f02f
David <davidkrdona7@hotmail.es>
2017-03-10 12:01:07 +0100

    Update my_io.c

--- Practica1/my_io.c
+++ Practica1/my_io.c
@@ -27,27 +27,3 @@ BYTE whereY(void){
 
 
 
-
-
-
-
-
-
-
-void selecciona_modo_video(BYTE modo)
-{
-union REGS inregs, outregs;
-inregs.h.ah = 0x00;
-inregs.h.al = modo;
-int86(0x10,&inregs,&outregs);
-return;
-}
-
-main(){
-int mode;
-scanf("%d",&mode);
-selecciona_modo_video(mode);
-scanf("%d",&mode);
-if(mode==3)	selecciona_modo_video(mode);
-return 0;
-}
